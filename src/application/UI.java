b6c378c5d7a2360e3bbdb23deaa9ca0a617833c8
Marco Chaves <macschaves@gmail.com>
2023-09-15 12:11:12 -0300

    Movendo as peças

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,7 +1,11 @@
 package application;
 
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
 
@@ -26,10 +30,22 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static ChessPosition readChessPosition (Scanner sc) {
+        try {
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        }
+        catch (RuntimeException e) {
+            throw new InputMismatchException("Erro lendo posicao do xadrez. Os valores válidos são de a1 a h8");
+        }
+    }
+
     public static void printBoard (ChessPiece[][] pieces){
-        for (int i = 0; i< pieces.length; i++){
+        for (int i = 0; i < pieces.length; i++){
             System.out.print((8 - i) + " ");
-            for (int j = 0; j< pieces.length; j++) {
+            for (int j = 0; j < pieces.length; j++) {
                printPiece(pieces [i][j]);
             }
             System.out.println();
@@ -38,13 +54,6 @@ public class UI {
     }
 
     private static void printPiece(ChessPiece piece) {
-        if (piece == null) {
-            System.out.print("-");
-        }
-        else {
-            System.out.print(piece);
-        }
-        System.out.print(" ");
         if (piece == null) {
             System.out.print("-");
         }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -30,6 +30,7 @@ public class Board {
             throw new BoardException("Posicao inexistente no tabuleiro");
         }
         return pieces [row][column];
+
     }
     public Piece piece (Position position) {
         if (!positionExists(position)){
@@ -47,9 +48,23 @@ public class Board {
         piece.position = position;
     }
 
+    public Piece removePiece (Position position){
+        if (!positionExists(position)){
+            throw new BoardException("Posicao inexistente no tabuleiro");
+        }
+        if (piece(position) == null) {
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces [position.getRow()] [position.getColumn()] = null;
+        return aux;
+    }
+
     private boolean positionExists( int row, int column) {
         return row >= 0 && row < rows && column >= 0 && column < columns;
     }
+
     public boolean positionExists(Position position){
         return positionExists(position.getRow(), position.getColumn());
     }
