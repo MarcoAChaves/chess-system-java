1600fc095da026f046bc19f4631b2e3889f639de
Marco Chaves <macschaves@gmail.com>
2023-09-15 07:51:37 -0300

    Colocando peças no tabuleiro

---
+++ src/chess/pieces/Rook.java
@@ -0,0 +1,21 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rook extends ChessPiece {
+    public Rook(Board board, Color color, Position position) {
+        super(board, color);
+    }
+
+    public Rook(Board board, Color white) {
+        super(board, white);
+    }
+
+    @Override
+    public String toString() {
+        return "R";
+    }
+}
