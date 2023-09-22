eed4e4520d552ec7a4d4f80a41d86329f4b2b080
Marco Chaves <macschaves@gmail.com>
2023-09-22 15:01:57 -0300

    Versão final

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -4,8 +4,6 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.*;
-
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -118,7 +116,7 @@ public class ChessMatch {
             throw new IllegalStateException("Nao a peca para ser promovida");
         }
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
-            throw new InvalidParameterException("Peca invalida para promocao");
+            return promoted;
         }
         Position pos = promoted.getChessPosition().toPosition();
         Piece p = board.removePiece(pos);
